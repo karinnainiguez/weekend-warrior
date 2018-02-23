3261243e7848be08fb840164d429897c5bce0b51
Karinna Iniguez <karinnainiguez@gmail.com>
2018-02-23 13:50:00 -0800

    Finished weekend-warrior-00

---
+++ 00-hello-world/hello_world.rb
@@ -0,0 +1,6 @@
+
+
+def hello_world(name)
+  name = "World" if name.empty?
+  return "Hello, #{name}!"
+end
--- 00-hello-world/hello_world_spec.rb
+++ 00-hello-world/hello_world_spec.rb
@@ -5,14 +5,14 @@ require_relative 'hello_world'
 
 describe "Hello World" do
   it "When given 'Alice' it should greet Alice!" do
-    expect(hello_world 'Alice').must_equal 'Hello, Alice!'
+    (hello_world 'Alice').must_equal 'Hello, Alice!'
   end
 
   it "When given 'Bob' it should greet Bob!" do
-    expect(hello_world 'Bob').must_equal 'Hello, Bob!'
+    (hello_world 'Bob').must_equal 'Hello, Bob!'
   end
 
   it "When given an empty string it should greet the world!" do
-    expect(hello_world '').must_equal 'Hello, World!'
+    (hello_world '').must_equal 'Hello, World!'
   end
 end
